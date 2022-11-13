0284ecec20e700e119ff9ea977ba8c1d1c1390b4
NEON <mgutie33@hotmail.com>
2022-11-13 11:29:33 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+       echo "11111";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" enctype="multipart/form-data">
+
+  <input id="cedula" type="text" name="cedula" size="13"
+         placeholder="cedula"
+      style="position: absolute; top: 200px; left: 100px;" />
+
+  <input id="telefono" type="text" name="telefono" size="13"
+         placeholder="telefono"
+      style="position: absolute; top: 250px; left: 100px;" />
+
+</form>
+
+</body>
+</html>
